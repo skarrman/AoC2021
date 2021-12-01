3d84752866575a29b1bc2d132ba1e62213ef1546
Simon Kärrman <simon.karrman@cygni.se>
2021-12-01 11:06:56 +0100

    Change structure a bit

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -14,31 +14,27 @@ fn main() {
     let data = get_data();
     let out_dir = env::var("part");
     match out_dir {
-        Ok(part) if part == "part1" => part1(&data),
-        Ok(part) if part == "part2" => part1(&data),
+        Ok(part) if part == "part1" => println!("{}", part1(&data)),
+        Ok(part) if part == "part2" => println!("{}", part2(&data)),
         _ => {
-            part1(&data);
-            part2(&data);
+            println!("{}", part1(&data));
+            println!("{}", part2(&data));
         }
     }
 }
 
-fn part1(data: &Vec<usize>) {
-    let res = data.as_slice().windows(2).fold(0, |sum, window| {
+fn part1(data: &Vec<usize>) -> usize {
+    data.windows(2).fold(0, |sum, window| {
         sum + if window[0] < window[1] { 1 } else { 0 }
-    });
-    println!("{}", res);
+    })
 }
 
-fn part2(data: &Vec<usize>) {
-    let res = data
-        .as_slice()
-        .windows(3)
-        .map(|window| window[0] + window[1] + window[2])
+fn part2(data: &Vec<usize>) -> usize {
+    data.windows(3)
+        .map(|window| window.iter().sum())
         .collect::<Vec<usize>>()
         .windows(2)
         .fold(0, |sum, window| {
             sum + if window[0] < window[1] { 1 } else { 0 }
-        });
-    println!("{}", res);
+        })
 }
