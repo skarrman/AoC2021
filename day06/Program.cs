e908dbe197da6b6ec987f822e2a8c5c7d3a02541
Simon Kärrman <simon.karrman@cygni.se>
2021-12-06 07:48:01 +0100

    Add solution to the "Day 6: Lanternfish" problem

---
+++ day06/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine(
+    Enumerable.Range(0, Environment.GetEnvironmentVariable("part") == "part1" ? 80 : 256)
+        .Aggregate(File.ReadAllText("input.txt").Split(",").Select(Int64.Parse).Aggregate(
+            Enumerable.Range(0, 9).ToDictionary(x => (long) x, _ => 0L), (d, f) =>
+            {
+                d[f]++;
+                return d;
+            }), (current, _) => Enumerable.Range(0, 9)
+            .ToDictionary(x => (long) x, x => x switch
+            {
+                6 => current[7] + current[0],
+                8 => current[0],
+                var n => current[n + 1]
+            })).Values.Sum());
