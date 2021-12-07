5134aae6f6f51801a0d193f39f83a801928d6670
Simon Kärrman <simon.karrman@cygni.se>
2021-12-07 14:49:57 +0100

    Make day07 code little bit shorter

--- day07/src/main/kotlin/AocTemplate.kt
+++ day07/src/main/kotlin/AocTemplate.kt
@@ -4,13 +4,9 @@ import kotlin.math.abs
 
 fun main() {
     val input = File("input.txt").readText().split(",").map { it.toInt() }
-    val answer = (0..input.maxOf { it }).fold(Int.MAX_VALUE) { m, i ->
-        min(m, input.fold(0) { s, p ->
-            s + when (System.getenv("part")) {
-                "part2" -> (abs(i - p) * (abs(i - p) + 1)) / 2
-                else -> abs(i - p)
-            }
-        })
-    }
-    println(answer)
+    println((0..input.maxOf { it }).fold(Int.MAX_VALUE) { m, i ->
+        min(m, input.fold(0) { s, p -> s + fuel(i, p) })
+    })
 }
+
+fun fuel(i: Int, p: Int): Int = if ((System.getenv("part")) == "part2") (abs(i - p) * (abs(i - p) + 1)) / 2 else abs(i - p)
